3af9831e54e562fd253dfa0fc44ef7a18221f253
Priya Shivanna <113131079+Priyashivanna19@users.noreply.github.com>
2022-09-08 13:43:45 -0600

    Update streamlit_app.py
    Edited Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
-streamlit.text('U+1F963, Omega 3 & Blueberry Oatmeal')
-streamlit.text('U+1F957, Kale, Spinach & Rocket Smoothie')
-streamlit.text(':Bread: HArd-Boiled Free-Range Eggs')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥙 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 HArd-Boiled Free-Range Eggs')
+Streamlit.text('🥑🍞 Avocado Toast'
