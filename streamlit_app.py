b9b3f2cec19cd1c5a8949a207e946fc04d026f24
Priya Shivanna <113131079+Priyashivanna19@users.noreply.github.com>
2022-09-13 11:52:42 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🍓 Build Your Own Fruit Smoothie 🥝🍇')
 
+
 #import pandas
 my_fruit_list  = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -23,7 +24,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
-
 
 #create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
